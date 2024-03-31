177ea290a6b60b2f938c6526454962ce436d6f0d
Nacho Cismondi <55794628+nachocismondi@users.noreply.github.com>
2024-03-31 18:04:09 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 100);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 300);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 1000);
+
+setTimeout(function () {
+  var msnry = new Masonry('.grid');
+  msnry.layout();
+}, 5000);
